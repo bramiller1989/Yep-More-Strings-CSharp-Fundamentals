36621853e7d517e89f43196abeb334fcd839ef82
bramiller1989 <bramiller1989@gmail.com>
2021-01-21 20:23:43 -0600

    add exercise 1

--- Strings.cs
+++ Strings.cs
@@ -1,10 +1,31 @@
-﻿namespace YepMoreStrings
+﻿using System;
+using System.Collections.Generic;
+
+namespace YepMoreStrings
 {
     public class Strings
     {
         public void Exercise1()
         {
-            throw new System.NotImplementedException();
+            Console.WriteLine("Enter a few numbers (eg 1-2-3-4): ");
+            var input = Console.ReadLine();
+
+            var numbers = new List<int>();
+            foreach (var number in input.Split('-'))
+                numbers.Add(Convert.ToInt32(number));
+
+            numbers.Sort();
+
+            var isConsecutive = true;
+            for (var i = 1; i < numbers.Count; i++)
+            {
+                if (numbers[i] == numbers[i - 1] + 1) continue;
+                isConsecutive = false;
+                break;
+            }
+
+            var message = isConsecutive ? "Consecutive" : "Not Consecutive";
+            Console.WriteLine(message);
         }
 
         public void Exercise2()
